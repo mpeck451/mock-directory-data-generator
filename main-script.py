5e10f46bab793d3e346f24492a333f50dc61e44e
Mason Peck <mpeck451@outlook.com>
2022-11-29 14:44:09 -0600

    Initialize variables and build a test listing.

---
+++ main-script.py
@@ -0,0 +1,21 @@
+import csv
+import random
+
+# 1 - Initialize Variables
+print("Script Start...")
+new_data = []
+number_of_listings = 0
+test_listing = {
+    'Primary Key': 1,
+    'Title': "MD",
+    'First Name': "John",
+    'Last Name': "Smith",
+    'Department': "Cardiology",
+    'Phone': 5551234567,
+    'SMS': 5551234567,
+    'Pager': 5559876543,
+    'Email': "jsmith1@fakehospital.org"
+}
+
+# Finalize
+print("...Script End")
