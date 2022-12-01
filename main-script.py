b710be39a36cd109df3124dc824a3f261f544c81
Mason Peck <mpeck451@outlook.com>
2022-12-01 13:30:00 -0600

    Add CSV module programming to generate a new TSV file.

--- main-script.py
+++ main-script.py
@@ -2,25 +2,16 @@ import csv
 import random
 import names
 import departments
-import json
+from datetime import datetime
 
 # 1 - Initialize Variables
 print("Script Start...")
+time_stamp = datetime.now().strftime("%m-%d-%Y-%H%M%S")
 new_data = []
 fields = ['Primary Key', 'First Name', 'Last Name', 'Department', 'Phone', 'SMS', 'Pager', 'Email']
-number_of_listings = 100
+number_of_listings = 3000
 pk_counter = 1001
 email_domain = 'fakehospital.org'
-test_listing = {
-    'primary_key': 1,
-    'first_name': "John",
-    'last_name': "Smith",
-    'department': "Cardiology",
-    'phone': 5551234567,
-    'sms': 5551234567,
-    'pager': 5559876543,
-    'email': "jsmith1@fakehospital.org"
-}
 
 # Generate New Listings
 while number_of_listings > 0:
@@ -28,19 +19,26 @@ while number_of_listings > 0:
     random_first_name = names.generate_name('first')
     random_last_name = names.generate_name('last')
     new_listing = {
-        'primary_key': pk_counter,
-        'first_name': random_first_name,
-        'last_name': random_last_name,
-        'department': departments.generate_department(),
-        'phone': cell_phone,
-        'sms': cell_phone,
-        'pager': '555' + str(random.randint(1000000, 9999999)),
-        'email': random_first_name[0].lower() + random_last_name.lower() + "@" + email_domain
+        'Primary Key': pk_counter,
+        'First Name': random_first_name,
+        'Last Name': random_last_name,
+        'Department': departments.generate_department(),
+        'Phone': cell_phone,
+        'SMS': cell_phone,
+        'Pager': '555' + str(random.randint(1000000, 9999999)),
+        'Email': random_first_name[0].lower() + random_last_name.lower() + "@" + email_domain
     }
     new_data.append(new_listing)
     number_of_listings -= 1
     pk_counter += 1
 print(new_data)
+
+# Create New TSV File
+with open(f'mock-data-{time_stamp}.tsv', 'w') as mock_data_file:
+    mock_data_writer = csv.DictWriter(mock_data_file, delimiter='\t', fieldnames=fields)
+    mock_data_writer.writeheader()
+    for listing in new_data:
+        mock_data_writer.writerow(listing)
 
 # Finalize
 print("...Script End")
